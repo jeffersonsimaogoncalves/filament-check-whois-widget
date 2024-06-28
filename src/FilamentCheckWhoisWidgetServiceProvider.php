5aa2287cdd98910feb7c075ba856ee57864f32b6
jeffersonsimaogoncalves <jeffersonsimaogoncalves@users.noreply.github.com>
2024-06-28 19:49:16 +0000

    Fix styling

--- src/FilamentCheckWhoisWidgetServiceProvider.php
+++ src/FilamentCheckWhoisWidgetServiceProvider.php
@@ -9,12 +9,12 @@ use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
+use JeffersonSimaoGoncalves\FilamentCheckWhoisWidget\Commands\FilamentCheckWhoisWidgetCommand;
+use JeffersonSimaoGoncalves\FilamentCheckWhoisWidget\Testing\TestsFilamentCheckWhoisWidget;
 use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use JeffersonSimaoGoncalves\FilamentCheckWhoisWidget\Commands\FilamentCheckWhoisWidgetCommand;
-use JeffersonSimaoGoncalves\FilamentCheckWhoisWidget\Testing\TestsFilamentCheckWhoisWidget;
 
 class FilamentCheckWhoisWidgetServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,10 +13,10 @@ use Filament\Support\SupportServiceProvider;
 use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use JeffersonSimaoGoncalves\FilamentCheckWhoisWidget\FilamentCheckWhoisWidgetServiceProvider;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use JeffersonSimaoGoncalves\FilamentCheckWhoisWidget\FilamentCheckWhoisWidgetServiceProvider;
 
 class TestCase extends Orchestra
 {
